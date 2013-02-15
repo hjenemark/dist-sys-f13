371d065532fab69c2ff4ca5a39a967907e710a4a
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-02-15 13:40:40 +0000

    Some layout changes and framework for temperature thread implementation.

--- sensor.h
+++ sensor.h
@@ -1,22 +1,2 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <arpa/inet.h>
-#include <string.h>
-
+#include "common.h"
 #include "temp_th.h"
-#include "admin_th.h"
-#include "data_th.h"
-
-#define SENSOR_SLEEP_INTERVAL 15
-
-#define USER_PROVIDED_IP 1
-#define OS_PROVIDED_IP 0
-
-struct network_params {
-	uint8_t net_mode;	
-	int domain;
-	unsigned char buf[sizeof(struct in6_addr)];
-	
-};
--- temp_th.h
+++ temp_th.h
@@ -1,1 +1,13 @@
+#include "common.h"
+
+#ifndef _TEMP_TH_HEADER
+#define _TEMP_TH_HEADER
+
+#define SENSOR_SLEEP_INTERVAL 15
+
 void *temperature_thread_entry(void *np);
+int8_t get_temperature();
+void send_temperature(struct network_params *np, int8_t temperature);
+int8_t get_admin_params(struct network_params *np);
+
+#endif
