c48f919c62cce72624aab97dfc1c958d3748d673
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-02-21 10:01:47 +0000

    Small changes in main program.

--- data_th.h
+++ data_th.h
@@ -1,1 +1,8 @@
+#include "common.h"
+
+#ifndef _DATA_TH_HEADER
+#define _DATA_TH_HEADER
+
 void *data_network_thread_entry(void *np);
+
+#endif
--- temp_th.c
+++ temp_th.c
@@ -1,5 +1,16 @@
 #include "temp_th.h"
 
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 void *temperature_thread_entry(void *np)
 {
 	int8_t temperature;
@@ -42,27 +53,34 @@ void send_temperature(struct network_params *np, struct peer_net_params *pnp,
 	printf("[Temp] Sending temperature \"%d\" to server \"%s\".\r\n", temperature, pnp->text_ip_addr);	
 	/* TODO: message encoding needs work */	
 	encode_message();	
-	int sock;
-	struct sockaddr_in tempsender;
-	char buffer[512];
-	uint32_t echolen;
-	uint32_t received = 0;
-	/* ATTN = UDP Socket !!!! */
-	if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-		perror("[Temp] Failed to create socket");
-		pthread_exit(NULL);
-	}
-	
-	memset(&tempsender, 0, sizeof(tempsender)); 	/* Clear struct */
-	tempsender.sin_family = np->domain; 		/* Internet/IP */
-	tempsender.sin_addr.s_addr = np->buf;		/* IP address */
-	tempsender.sin_port = DATA_PORT; 		/* server port */
-	
-	if (connect(
-		sock, (struct sockaddr *) &tempsender, sizeof(tempsender)) < 0) {
-		perror("[Temp] Failed to connect with server");
-		pthread_exit(NULL);
+
+
+	int sockfd, numbytes;  
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(pnp->text_ip_addr, DATA_PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
 	}
 
-	send(sock, temperature, sizeof(temperature), 0);
+	sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)		
+	connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen)
+
+	inet_ntop(servinfo->ai_family, get_in_addr((struct sockaddr *)servinfo->ai_addr),
+	    s, sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	
+	if (send(sockfd, "Hello, world!", 13, 0) == -1) perror("send");
+
+	close(sockfd);
 }
