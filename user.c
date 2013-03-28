025cc58c7b92a00b240e81d3365eaeedb9b26868
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-03-28 21:25:13 +0000

    User interface program work-in-progress. Can connect to server.

--- user.c
+++ user.c
@@ -1,47 +1,71 @@
 /**
  * \file user.c
  **/
-
+#include <stdbool.h>
 #include "common.h"
 
-int connect_to_node()
+uint32_t connect_to_node(struct peer_net_params *pnp, uint32_t *socket)
+{
+	printf("Enter node IP address:\r\n");
+	scanf("%s", pnp->ipstr);
+
+	struct network_params np;
+
+	np.net_mode = OS_PROVIDED_IP;
+	pnp->family = AF_INET;
+	
+	*socket = get_socket(&np, pnp, DATA_SUBMIT);
+	return 0;
+}
+uint32_t promote_node(uint32_t socket)
 {
 	return 0;
 }
-int promote_node() 
+uint32_t get_avg_temp(uint32_t socket)
 {
 	return 0;
 }
-int get_avg_temp() 
+
+int32_t main ()
 {
-	return 0;
-}
-int main (int argc, char *argv[])
-{
+	bool admin_connected = false;
+	uint32_t socket;
+	struct peer_net_params pnp;
+	
 	printf("User node program.\r\n");
 	while (1) {
+		admin_connected ? 
+			printf("Connected to: %s\r\n", pnp.ipstr) : 
+			printf("Connected to: NONE\r\n") ;
 		printf("Select task:\r\n");
-		printf("[1] Connect to Admin Node.\r\n");
-		printf("[2] Promote node to Admin node.\r\n");
-		printf("[3] Get average T from Admin node.\r\n");
-		printf("[4] Quit.\r\n");
+		printf("[1] Quit.\r\n");
+			printf("[2] Connect to node.\r\n");
+		if (admin_connected) {
+			printf("[3] Promote node to Admin mode.\r\n");
+			printf("[4] Get average T from node.\r\n");
+		}
+		
 
-		int option;
+		uint32_t option;
 		scanf("%d", &option);
 
 		switch(option) {
 			case 1:
-				connect_to_node();
-				break;
-			case 2:
-				promote_node();
-				break;
-			case 3:
-				get_avg_temp();
-				break;
-			case 4:
+				if (admin_connected) close(socket);
 				exit(EXIT_SUCCESS);
 				break;
+			case 2:
+				if (admin_connected) close(socket);
+				connect_to_node(&pnp, &socket);
+				admin_connected = true;
+				break;
+			case 3:
+				promote_node(socket);
+				break;
+			case 4:
+				get_avg_temp(socket);
+				break;
+				
 		}
 	}
 }
