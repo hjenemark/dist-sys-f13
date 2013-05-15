b97c7b7ee69982d02ff0baa3ae2e8cda99b2ac2f
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-15 18:26:59 +0000

    Master's IP is set to node's IP when node is promoted to master.

--- data_th.h
+++ data_th.h
@@ -6,6 +6,6 @@
 void *data_network_thread_entry(void *np);
 void report_average_temperature(int32_t socket);
 void update_promo_key(char *keydata);
-void become_master();
+void become_master(struct network_params *np);
 
 #endif
