a787efb54c2bd9d598e56292eb523d80b133ac93
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-15 14:46:34 +0000

    Communication between user and sensor works fine - tested.

--- trunk/data_th.h
+++ trunk/data_th.h
@@ -4,5 +4,8 @@
 #define _DATA_TH_HEADER
 
 void *data_network_thread_entry(void *np);
+void report_average_temperature(int32_t socket);
+void update_promo_key(char *keydata);
+void become_master();
 
 #endif
