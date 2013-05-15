69bcad12b2318a214a2855957c21b0b658d83062
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-15 17:35:42 +0000

    User node program. All functions tested and working fine.

--- trunk/user.c
+++ trunk/user.c
@@ -14,7 +14,7 @@ uint32_t connect_to_node(struct peer_net_params *pnp, uint32_t *socket)
 	np.net_mode = OS_PROVIDED_IP;
 	pnp->family = AF_INET;
 	
-	*socket = get_socket(&np, pnp, DATA_SUBMIT);
+	if((*socket = get_socket(&np, pnp, DATA_SUBMIT)) == -1) return -1;
 	return 0;
 }
 uint32_t promote_node(uint32_t socket, uint32_t *promokey)
@@ -32,9 +32,15 @@ uint32_t promote_node(uint32_t socket, uint32_t *promokey)
 	msg = serilization(node_msg);
 	int32_t len = strlen(msg);
 
-	send(socket, msg, len, 0);
+	int32_t sentbytes;
+	sentbytes = send(socket, msg, len, 0);
+	if (sentbytes != len) {
+		printf("Unable to send message. Socket will be closed");
+		close(socket);
+		return 1;
+	}
+	
 	(*promokey)++;
-
 	return 0;
 }
 uint32_t get_avg_temp(uint32_t socket)
@@ -50,14 +56,19 @@ uint32_t get_avg_temp(uint32_t socket)
 	send(socket, msg, len, 0);
 
 	char buff[256];
-	recv(socket, buff, 256, 0);
-
+	int32_t rx_bytes;
+	if((rx_bytes=recv(socket, buff, 256, 0))==0) {
+		printf("Remote end closed connection!\r\n");
+		return 1;
+	}
+	buff[rx_bytes] = '\0';
+	
 	struct node_message* rx_msg;
 	rx_msg = deserialize(buff);
 
     while (rx_msg != NULL) {
 		if (rx_msg->operation == REPORT_AVG_TEMP)
-			printf("Average temperature: %d",atoi(rx_msg->operand));
+			printf("Average temperature: %d\r\n",atoi(rx_msg->operand));
 		rx_msg = rx_msg->next;
 	}
 	
@@ -66,16 +77,24 @@ uint32_t get_avg_temp(uint32_t socket)
 
 int32_t main ()
 {
-	bool admin_connected = false;
-	uint32_t socket, option;
-	uint32_t promokey = 1;
+	bool admin_connected = false, wait;
+	uint32_t socket, promokey = 1, error = 0, retval; 
+	char option;
 	struct peer_net_params pnp;
-	
+
+	struct timespec tv;
+	tv.tv_sec = 3;
+	tv.tv_nsec = 0;
+
+	fd_set readfds;
+	FD_ZERO(&readfds);
+
 	printf("User node program.\r\n");
 	while (1) {
 		admin_connected ? 
 			printf("Connected to: %s\r\n", pnp.ipstr) : 
 			printf("Connected to: NONE\r\n") ;
+
 		printf("Select task:\r\n");
 		printf("[1] Quit.\r\n");
 		printf("[2] Connect to node.\r\n");
@@ -83,25 +102,51 @@ int32_t main ()
 			printf("[3] Promote node to Admin mode.\r\n");
 			printf("[4] Get average T from node.\r\n");
 		}
-		
-		scanf("%d", &option);
+
+		wait = true;
+		while(wait) {
+			FD_SET(STDIN_FILENO, &readfds);
+			pselect(STDIN_FILENO+1, &readfds, NULL, NULL, &tv, NULL);
+			if(FD_ISSET(STDIN_FILENO, &readfds)) {
+				scanf("%c", &option);
+				wait = false;
+			} else if(admin_connected) {
+				socklen_t len = sizeof (error);
+				retval = getsockopt (socket, SOL_SOCKET, SO_ERROR, &error, &len );
+				if(retval != 0) {
+					printf("Socket is broken!\r\n");
+					admin_connected = false;
+					wait = false;
+					option = 'x';
+				}
+			}
+		}
+
 		switch(option) {
-			case 1:
+			case '1':
 				if (admin_connected) close(socket);
 				exit(EXIT_SUCCESS);
 				break;
-			case 2:
+			case '2':
 				if (admin_connected) close(socket);
-				connect_to_node(&pnp, &socket);
-				admin_connected = true;
+				if(connect_to_node(&pnp, &socket) == -1) {
+					printf("Connecting failed!\r\n");
+					admin_connected = false;
+				}
+				else
+					admin_connected = true;
 				break;
-			case 3:
-				promote_node(socket, &promokey);
+			case '3':
+				if(promote_node(socket, &promokey)==1) admin_connected = false;
 				break;
-			case 4:
-				get_avg_temp(socket);
+			case '4':
+				if(get_avg_temp(socket)==1) admin_connected = false;
 				break;
-				
+			case 'x':
+				break;
+			default:
+				printf("Unrecognized command!\r\n");
+				break;
 		}
 	}
 }
