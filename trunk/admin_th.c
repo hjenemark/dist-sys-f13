91d391d40702994e78e711ed6aeb222c9ac09731
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-24 19:16:56 +0000

    Admin thread cleaned up and docummented

--- trunk/admin_th.c
+++ trunk/admin_th.c
@@ -1,16 +1,12 @@
 /**
  * \file admin_th.c
  *
- * Worker functions for the admin thread
  **/
 
 #include "common.h"
 #include "admin_th.h"
 #include "globals.h"
 
-/**
- * \brief Admin thread main function
- **/
 void *admin_network_thread_entry()
 {
 	printf("[Admin] Starting admin thread!\r\n");
@@ -36,12 +32,6 @@ void *admin_network_thread_entry()
 	pthread_exit(NULL);
 }
 
-/**
- * \brief Does the work of the admin thread.
- *
- * If the node is the admin a message is broadcast on the network.
- * Otherwise it waits for the promote message.
- **/
 void recurse_worker(int32_t socket)
 {
 	int32_t cpy_node_master, cpy_node_masterid;
@@ -54,33 +44,33 @@ void recurse_worker(int32_t socket)
 
 	if (cpy_node_master) {
 		/* Node is master */
+		/* Prepare local variable */
 		int32_t buflen, numbytes;
 		char buffer[15];
 		struct node_message *node_msg = NULL;
-		struct addrinfo hints, *servinfo;
 		struct sockaddr_in their_addr;
 
-		printf("ADMIN DEBUG: Node is master. Broadcasting.\r\n");
+		printf("[ADMIN DEBUG]: Node is master. Broadcasting.\r\n");
+
+		/* Preprare message containing admin IP address */
 		char ipstr[INET6_ADDRSTRLEN];
 		strcpy (ipstr,np.ipstr);
 		int32_t addrlen = strlen(ipstr);
-		printf("ADMIN DEBUG: Master IP Len: %d IP: %s\r\n", addrlen, ipstr);
-		
+		printf("[ADMIN DEBUG]: Master IP Len: %d IP: %s\r\n", addrlen, ipstr);
 		add_node_msg (&node_msg, ANNOUNCE_MASTER, addrlen, ipstr);
+
+		/* Preprare message containing current admin ID number */
 		buflen = snprintf(buffer, 15, "%d", cpy_node_masterid);
 		add_node_msg (&node_msg, PROMO_KEY, buflen, buffer);
 
-		char *msg=NULL, port_str[5];
+		/* Serialize messages */
+		char *msg=NULL;
 		msg = serilization(node_msg);
 		int32_t len = strlen(msg);
 
-		/*memset(&hints, 0, sizeof hints);
-		hints.ai_family = AF_UNSPEC;
-		hints.ai_socktype = SOCK_DGRAM;
-		sprintf(port_str, "%d", ADMIN_PORT);
-		getaddrinfo("255.255.255.255", port_str, &hints, &servinfo);*/
-		their_addr.sin_family = AF_INET;     // host byte order
-		their_addr.sin_port = htons(51002); // short, network byte order
+		/* Send data to broadcast address */
+		their_addr.sin_family = AF_INET;
+		their_addr.sin_port = htons(ADMIN_PORT);
 		struct hostent *he;
 		he=gethostbyname("10.10.10.255");
 		their_addr.sin_addr = *((struct in_addr *)he->h_addr);
@@ -88,19 +78,15 @@ void recurse_worker(int32_t socket)
 
 		if ((numbytes=sendto(socket, msg, len, 0,
              (struct sockaddr *)&their_addr, sizeof their_addr)) == -1) {
-   			perror("sendto");
+   			perror("[Admin] sendto");
     		exit(1);
 		}
 		
-		/*if ((numbytes = sendto(socket, msg, len, 0,
-             servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
-    			perror("admin-th: sendto");
-    			exit(1);
-		}*/
-		printf("[Admin] Broadcast beacon size %d sent!\r\n",numbytes);
+		printf("[Admin] Broadcast beacon of size %d sent!\r\n", numbytes);
 		sleep(ADMIN_SLEEP_INTERVAL);
 	} else {
 		/* Node is slave */
+		/* Prepare local variables */
 		struct timeval tv;
 		fd_set readfds;
 
@@ -110,18 +96,26 @@ void recurse_worker(int32_t socket)
 		FD_ZERO(&readfds);
 		FD_SET(socket, &readfds);
 
+		/* Wait for data or SLEEP interval whichever first */
 		select(socket+1, &readfds, NULL, NULL, &tv);
 
+		/* Check if data was received */
 		if(FD_ISSET(socket, &readfds)) {
 			/* Some data received */
 			socklen_t addr_len;
 			struct sockaddr_storage their_addr;
 			addr_len = sizeof their_addr;
-			int numbytes;
+			int32_t numbytes;
 			char s[INET6_ADDRSTRLEN];
-		
 			char buf[100];
-		
+
+			/* Preprate temporaty variables */
+			char temp_ip[INET6_ADDRSTRLEN];
+			temp_ip[0]='0';
+			int32_t temp_master_id=-1;
+			int32_t temp_af_family;
+
+			/* Receive data from socket */
 			if ((numbytes = recvfrom(socket, buf, 100-1 , 0,
 		    (struct sockaddr *)&their_addr, &addr_len)) == -1) {
 	   			perror("recvfrom");
@@ -133,29 +127,45 @@ void recurse_worker(int32_t socket)
         		get_in_addr((struct sockaddr *)&their_addr),
         		s, sizeof s));
 
-			char ipstr[INET6_ADDRSTRLEN];
-			struct sockaddr_in *st = (struct sockaddr_in *)&their_addr;
-			inet_ntop(AF_INET, &st->sin_addr, ipstr, sizeof ipstr);
-			printf("[DEBUG] REAL Sensor IP address: %s\n", ipstr);
-
+			/* Parse received messages */
 			node_msg* rx_msg;
 			rx_msg = deserialize(buf);
 			print_node_list_msg(rx_msg);
 			while(rx_msg != NULL) {
 				switch(rx_msg->operation) {
 					case ANNOUNCE_MASTER:
-						pthread_mutex_lock (&mutex_adminp);
-						strncpy(admin_net_params.ipstr, rx_msg->operand, rx_msg->op_size);
-						admin_net_params.family=their_addr.ss_family;
-						pthread_mutex_unlock (&mutex_adminp);
-						printf("[Admin]: New master announcement:  %s\n", admin_net_params.ipstr);
+						/* Store admin node's IP address to tem variable */
+						printf("[Admin]: New master announcement:  %s\n", rx_msg->operand);
+						strncpy(temp_ip, rx_msg->operand, rx_msg->op_size);
+						temp_af_family = their_addr.ss_family;
+						if(temp_master_id != -1) {
+							/* Master announcement is valid - save it */
+							printf("[Admin] New valid master announcment!\r\n");
+							pthread_mutex_lock (&mutex_adminp);
+							admin_net_params.family=temp_af_family;
+							strncpy(admin_net_params.ipstr, temp_ip, strlen(temp_ip));
+							current_master_id = temp_master_id;
+							pthread_mutex_unlock (&mutex_adminp);
+							temp_master_id=-1;
+							temp_ip[0] = '0';
+						}
 						break;
 					case PROMO_KEY:
-						pthread_mutex_lock (&mutex_adminp);
-						current_master_id=atoi(rx_msg->operand);
-						pthread_mutex_unlock (&mutex_adminp);
-						printf("[Admin]: New master id:  %d\n", 
-						   	 atoi(rx_msg->operand));
+						printf("[Admin]: New master ID announcement:  %d\n", atoi(rx_msg->operand));
+						/* Check if received id is higher than curent */
+						if (current_master_id < atoi(rx_msg->operand))
+							temp_master_id= atoi(rx_msg->operand);
+						if(temp_ip[0] != '0') {
+							/* Master announcement is valid - save it */
+							printf("[Admin] New valid master announcment!\r\n");
+							pthread_mutex_lock (&mutex_adminp);
+							admin_net_params.family=temp_af_family;
+							strncpy(admin_net_params.ipstr, temp_ip, strlen(temp_ip));
+							current_master_id = temp_master_id;
+							pthread_mutex_unlock (&mutex_adminp);
+							temp_master_id=-1;
+							temp_ip[0] = '0';
+						}
 						break;
 				}
 				rx_msg = rx_msg->next;
--- trunk/admin_th.h
+++ trunk/admin_th.h
@@ -9,10 +9,26 @@
 #ifndef __ADMIN_TH_HEADER
 #define __ADMIN_TH_HEADER
 
-#define ADMIN_SLEEP_INTERVAL 10
-#define BACKLOG 100
+#define ADMIN_SLEEP_INTERVAL 10 /*!< Sleep interval of admin node */
+#define BACKLOG 100 /*!< Max number of connections to queue */
 
+/** 
+ * \brief Entry function of admin thread.
+ *
+ * \return void
+ **/
 void *admin_network_thread_entry();
+
+/** 
+ * \brief Worker function of admin thread.
+ * 
+ * If the node is the admin a message is broadcast on the network.
+ * Otherwise it waits for the promote message.
+ * 
+ * \param socket Broadcast socket to use when communicating.
+ *
+ * \return void
+ **/
 void recurse_worker(int32_t socket);
 
 #endif
