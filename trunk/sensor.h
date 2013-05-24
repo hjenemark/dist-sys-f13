3f294ddb1d5889727244e7825cdfee42bd7483a6
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-24 20:10:04 +0000

    Sensor documentation

--- trunk/sensor.h
+++ trunk/sensor.h
@@ -6,3 +6,33 @@
 #include "data_th.h"
 #include "admin_th.h"
 
+
+/** 
+ * \brief Checks validity of startup parameters
+ *
+ * \param argc Integer indicating number of parameters.
+ * 
+ * \param argv[] Pointer to a lits of passed parameters
+ *
+ * \return void
+ *
+ **/
+void check_startup_params(int argc, char *argv[]);
+
+/** 
+ * \brief Ensures prompt removal of dead processes
+ *
+ * \param s Unused
+ *
+ * \return void
+ *
+ **/
+void sigchld_handler(int s);
+
+/** 
+ * \brief Main entry function
+ *
+ * \return EXIT code
+ *
+ **/
+int main (int argc, char *argv[]);
