1941e631cfcb8d63972db8f2a17fba13937bfaa7
justas.poderys@gmail.com <justas.poderys@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-24 20:10:04 +0000

    Sensor documentation

--- sensor.c
+++ sensor.c
@@ -1,7 +1,5 @@
 /**
  * \file sensor.c
- * The sensor main thread.
- * Checks the parameters and starts the worker threads.
  **/
 #include <signal.h>
 #include <sys/types.h>
@@ -10,45 +8,33 @@
 #include "globals.h"
 
 
-int32_t current_master_id = 0;
-int32_t node_is_master = 0;
-pthread_mutex_t mutex_master_params;
+int32_t current_master_id = 0; /*!< Current master ID global */
+int32_t node_is_master = 0; /*!< Is current node a master */
+pthread_mutex_t mutex_master_params; /*!< Mutex to protect above parameters */
 
-struct peer_net_params admin_net_params;
-pthread_mutex_t mutex_adminp;
+struct peer_net_params admin_net_params; /*!< Admin node IP parameters */
+pthread_mutex_t mutex_adminp; /*!< Mutex to protect admin node IP parameters */
 
-int32_t node_admin_offset = 0;
-pthread_mutex_t mutex_timeoffset;
+int32_t node_admin_offset = 0; /*!< Local to node time offset */
+pthread_mutex_t mutex_timeoffset; /*!< Mutex to protect time offset */
 
-struct network_params np;
+struct network_params np; /*!< Struct holding local IP parameters */
 
 void sigchld_handler(int s)
 {
     while(waitpid(-1, NULL, WNOHANG) > 0);
 }
-/** 
- * \brief Checks startup parameters
- *
- * \param argc Integer indicating number of parameters.
- * 
- * \param argv[] Pointer to a lits of passed parameters.
- *
- * \param np A struct for storing parsed parameters.
- **/
+
 void check_startup_params(int argc, char *argv[])
 {
-	/**
-     * No parameters provided. Default OS parameters will be used.
-     */
+	/* No parameters provided. Default OS parameters will be used.*/
 	if (argc == 1) {
 		np.net_mode = OS_PROVIDED_IP;
 		printf("OS Provided IP will be used.\r\n");
 		return;
 	}
 
-	/**
-     * Update network parameters struct with IP version.
-     */
+	/* Update network parameters struct with IP version. */
 	int s;
 	np.net_mode = USER_PROVIDED_IP;
 	printf("User provided IP will be used.\r\n");	
@@ -60,9 +46,7 @@ void check_startup_params(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	/**
-     * Check that provided IP is consistent with IP version.
-     */
+	/* Check that provided IP is consistent with IP version. */
 	strcpy(np.ipstr, argv[2]);
 	char buff[INET6_ADDRSTRLEN];
 	s = inet_pton(np.family, argv[2], buff);
@@ -77,35 +61,36 @@ void check_startup_params(int argc, char *argv[])
 
 int main (int argc, char *argv[])
 {
-    struct sigaction sa;
 	np.ipstr[0] = '\0';
 	
 	printf("Sensor process implementation\r\n");
 	printf("HJE & JP. DTU 2013\r\n");
 
 	/* Check for start-up paramas */	
-	
 	if(!(argc == 1 || argc == 3)) {
 		printf("Usage: %s [{v4|v6} IP-Address]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-
 	check_startup_params(argc, argv);
 
-	/* Init mutexes */
+	/* Initialize mutexes */
 	pthread_mutex_init(&mutex_adminp, NULL);
 	pthread_mutex_init(&mutex_timeoffset, NULL);
 	pthread_mutex_init(&mutex_master_params, NULL);
 	
+
+	/* Remove orphaned and dead processes */
+	struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1){
+        perror("sigaction");
+        exit(1);
+    }
+
+	/* Start working threads */
 	pthread_t threads[3];
-	
-        sa.sa_handler = sigchld_handler;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = SA_RESTART;
-        if(sigaction(SIGCHLD, &sa, NULL) == -1){
-            perror("sigaction");
-            exit(1);
-        }
 	if(pthread_create(
 			&threads[0], NULL, 
 			temperature_thread_entry, NULL)) {
@@ -127,7 +112,7 @@ int main (int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	/* Loop while sensor is alive */
+	/* Loop while sensor is alive. Sleep to conserve CPU */
 	while (1) {
 		sleep(1);
 	}
