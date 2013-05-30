99876bcc18e9d02a460cc0fbdd0c143b9106d627
hjenemark@gmail.com <hjenemark@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-30 10:05:27 +0000

    runMultiple.sh now sets up the given number of ips and starts a sensor on each of the new ips

--- runMultiple.sh
+++ runMultiple.sh
@@ -2,11 +2,16 @@
 
 if [ $# -eq 0 ]
 then
-    echo "Usage: ./runMultible.sh <# of instances> <client>"
+    echo "Usage: ./runMultible.sh <client> <ip-start> <ip-end>"
     exit 1
 fi
-for (( i=0; i < $1; i++ ));
+for (( i=$2; i < $3; i++ )) ;
 do
-    xterm -T "Terminal $i" -e ./$2 $i &
+    sudo ifconfig eth0:$i 10.10.10.$i netmask 255.255.255.0
+done
+
+for (( i=$2; i < $3; i++ ));
+do
+    screen -d -m terminator -T "Terminal $i" -x ./$1 v4 10.10.10.$i
 done
 
