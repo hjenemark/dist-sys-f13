67c2550f6723677ff29f80ed86d9b5c89a84a566
hjenemark@gmail.com <hjenemark@gmail.com@02fd4090-6929-8cab-5efc-6d2548c18470>
2013-05-30 10:22:40 +0000

    now runMultiple.sh starts with eth0 and then moves onwards

--- trunk/runMultiple.sh
+++ trunk/runMultiple.sh
@@ -5,7 +5,10 @@ then
     echo "Usage: ./runMultible.sh <client> <ip-start> <ip-end>"
     exit 1
 fi
-for (( i=$2; i < $3; i++ )) ;
+i=$2
+sudo ifconfig eth0 10.10.10.$2 netmask 255.255.255.0
+
+for (( i=$i+1;i < $3; i++ )) ;
 do
     sudo ifconfig eth0:$i 10.10.10.$i netmask 255.255.255.0
 done
